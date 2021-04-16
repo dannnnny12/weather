258437f60356c41a0f89263d8456a73ca80b14d0
Danny <behappydanny1@gmail.com>
2021-04-16 22:50:08 +0800

    first commit

---
+++ App.vue
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content ="width-device">
+    <title>Practice</title>
+</head>
+<body>
+    <div id="app">
+        <div class="box">
+            {{message}}
+        </div>
+    </div>
+
+
+    <script src="https://unpkg.com/vue@2.6.12/dist/vue.js"></script>
+</body>
+</html>
